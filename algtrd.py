cbf608bed1b9697d2da69b1ea8247a1235b05060
gph03n1x <giannispapcod7@gmail.com>
2017-12-13 23:48:28 +0200

    Changed the images/ structure. Completed algtrd.py and experimentation.py . Created an example input

--- algtrd.py
+++ algtrd.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
 import math
+import json
 import random
-import unittest
 import argparse
 from copy import copy
 from collections import defaultdict
@@ -12,21 +11,37 @@ from collections import defaultdict
 
 class AllPayAuction:
     def __init__(self, values, budgets, random_start=True):
+        """
+        Αρχικοποιεί την δημοπρασία (διαγωνισμό) με προσφορές για τον κάθε πλειοδότη.
+        Η αρχική προσφορά μπορεί είτε να ξεκινάει για όλους από το 0 είτε να είναι τυχαία.
+        :param values:
+        :param budgets:
+        :param random_start:
+        """
         self.k = len(values)
         self.bidders = len(budgets)
         self.values = values
         self.budgets = budgets
+
         if random_start:
             self.bids = [(user_id, random.randint(0, self.budgets[user_id])) for user_id in range(self.bidders)]
         else:
             self.bids = [(user_id, 0) for user_id in range(self.bidders)]
-        print(self.bids)
 
-    def iterative_best_response(self, return_bids=True):
-        plot_data = defaultdict(list)
-        for bidder in range(self.bidders):
-            plot_data[bidder].append(self.bids[self.find_bid(bidder)][1])
-        count = 1
+    def iterative_best_response(self, return_bids=False):
+        """
+        Υλοποίηση του IBR. Σε κάθε iterate αφαιρεί έναν πλειοδότη από την λίστα των προσφορών ο οποίος μέσα από την
+        μέθοδο user_action αποφασίζει την επόμενη του προσφορά. Στην συνέχεια γίνεται μια ταξινόμηση των προσφορών.
+        Αν οι προσφορές δεν αλλάξουν σε έναν κύκλο είναι ένδειξη ότι έχουμε ισορροπία Nash και while loop σταματάει.
+        :param return_bids: boolean, ενδυκνύει αν θέλει να επιστρέψει τις αλλαγές των bids ανά iteration
+        :return:
+        """
+        if return_bids:
+            plot_data = defaultdict(list)
+            for bidder in range(self.bidders):
+                plot_data[bidder].append(self.bids[self.find_bid(bidder)][1])
+            count = 1
+
         while True:
 
             old_bids = copy(self.bids)
@@ -42,7 +57,7 @@ class AllPayAuction:
                     position = self.find_bid(bidder)
                     plot_data[bidder].append(self.bids[position][1])
 
-                count+=1
+                count += 1
 
             if old_bids == self.bids:
                 break
@@ -55,33 +70,55 @@ class AllPayAuction:
                min(self.bids, key=lambda bid: bid[1])[1]
 
     def user_action(self, user_id, previous_bid):
+        """
+        Ο χρήστης έχει μια σειρά από αποφάσεις από τις οποίες διαλέγει εκείνη που του αποδίδει το μεγαλύτερο
+        utility. Η αποφάσεις του λαμβάνονται σε σχέση με το κάθε τρόπαιο.
+        :param user_id:
+        :param previous_bid:
+        :return:
+        """
 
         # utility, bid
         attempts = [(0, 0)]
 
         for i in range(self.k):
-
+            # Κοιτάει αν η προσφορά του ξεπερνάει την προσφορά του πλειοδότη που κερδίζει το συγκεκριμένο τρόπαιο.
             if previous_bid > self.bids[i][1]:
                 attempts.insert(0, (self.utility_function_targeted(self.bids[i][1], user_id, i), previous_bid))
 
+            # Αν όχι και το id του είναι μεγαλύτερο από το id του πλειοδότη που κερδίζει κάνει την προσφορά του ίση με
+            # με την προσφορά του άλλου.
             elif self.bids[i][1] <= self.budgets[user_id] and self.bids[i][1] <= self.values[i] \
                     and self.bids[i][0] > user_id:
-                # print("using id to overcome")
+
                 attempts.insert(0, (self.utility_function_targeted(self.bids[i][1], user_id, i), self.bids[i][1]))
 
+            # Αν πάλι δεν έχει πιο δυνατο id τότε προσφέρει 1 παραπάνω αξία σε σχέση με τον άλλο.
             elif self.bids[i][1] + 1 <= self.budgets[user_id] and self.bids[i][1] + 1 <= self.values[i]:
-                # print("using budget to overcome")
                 attempts.insert(0, (self.utility_function_targeted(self.bids[i][1], user_id, i), self.bids[i][1] + 1))
 
-        # print("ID", user_id, "FA", attempts, "RE", max(attempts, key=lambda attempt: attempt[0]))
+        # Επιστρέφει την προσφορά που του δίνει το πιο πολύ utility.
         return max(attempts, key=lambda attempt: attempt[0])[1]
 
     def find_bid(self, user_id):
+        """
+        Βρίσκει την προσφορά ενός χρήστη με βάση το id του.
+        :param user_id:
+        :return:
+        """
         for position, bid in enumerate(self.bids):
             if bid[0] == user_id:
                 return position
 
     def utility_function_targeted(self, bid, user_id, value_id):
+        """
+        Επιστρέφει το utility ενός πλειδότη εφόσον τα λεφτά που θα προσφέρει στο τέλος
+        είναι μικρότερα της αξίας του τρόπαιου που θέλει να αποκτήσει.
+        :param bid:
+        :param user_id:
+        :param value_id:
+        :return:
+        """
         if bid > self.budgets[user_id]:
             return -math.inf
         return self.values[value_id] - bid
@@ -89,4 +126,21 @@ class AllPayAuction:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Algorithmic trading iterative best response.")
+    parser.add_argument("-d", "--data")
+    parser.add_argument("-rs", "--random-start", action="store_true", default=False)
+
     args = parser.parse_args()
+
+    if not args.data:
+        parser.print_help()
+
+    with open(args.data, "r") as data_file:
+        json_data = json.load(data_file)
+        for pair in json_data["values"]:
+            au = AllPayAuction(pair, json_data["budgets"], random_start=args.random_start)
+            max_effort, avg_effort, min_effort = au.iterative_best_response()
+            print("Maximum effort: ", max_effort)
+            print("Average effort: ", avg_effort)
+            print("Minimum effort: ", min_effort)
+            for bid in au.bids:
+                print("User id:{0} made a bid of {1}".format(bid[0], bid[1]))
--- experimentation.py
+++ experimentation.py
@@ -4,34 +4,39 @@ import matplotlib.pylab as plt
 
 
 class TestBiddingAllPayAuction(unittest.TestCase):
+    RANDOM_START = True
+    PATH = ""
+
     def setUp(self):
         print()
 
     def test_scenario_1(self):
-        self.name = "images/scenario1"
+        self.name = "scenario1/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [1000, 1000, 1000, 1000]
 
     def test_scenario_2(self):
-        self.name = "images/scenario2"
+        self.name = "scenario2/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [1000, 1000/2, 1000/2, 1000/2]
 
     def test_scenario_3(self):
-        self.name = "images/scenario3"
+        self.name = "scenario3/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [1000, 1000/10, 1000/10, 1000/10]
 
     def test_scenario_4(self):
-        self.name = "images/scenario4"
+        self.name = "scenario4/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [100+i*10 for i in range(4)]
 
     def tearDown(self):
-        for values in self.tests:
-            au = AllPayAuction(values, self.budgets, random_start=False)
+        self.base = "images/" + self.PATH
 
-            x_values, plot_data = au.iterative_best_response()
+        for values in self.tests:
+            au = AllPayAuction(values, self.budgets, random_start=self.RANDOM_START)
+
+            x_values, plot_data = au.iterative_best_response(True)
             x_values = range(x_values)
 
             ax1 = plt.subplot2grid((2, 2), (0, 0))
@@ -43,34 +48,42 @@ class TestBiddingAllPayAuction(unittest.TestCase):
             ax2.plot(x_values, plot_data[1], 'b')
             ax3.plot(x_values, plot_data[2], 'g')
             ax4.plot(x_values, plot_data[3], 'y')
-            plt.savefig(self.name+"-0start-"+str(values)+'.png')
+
+            plt.xlabel("Iterations")
+            plt.ylabel("Bid")
+
+            plt.savefig(self.base + self.name + str(values) + '.png')
 
 
 class TestMeasureAllPayAuctionEffort(unittest.TestCase):
+    RANDOM_START = True
+    PATH = ""
+
     def setUp(self):
         print()
 
     def test_scenario_1(self):
-        self.name = "images/scenario1"
+        self.name = "scenario1/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [1000, 1000, 1000, 1000]
 
     def test_scenario_2(self):
-        self.name = "images/scenario2"
+        self.name = "scenario2/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [1000, 1000/2, 1000/2, 1000/2]
 
     def test_scenario_3(self):
-        self.name = "images/scenario3"
+        self.name = "scenario3/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [1000, 1000/10, 1000/10, 1000/10]
 
     def test_scenario_4(self):
-        self.name = "images/scenario4"
+        self.name = "scenario4/"
         self.tests = [(999, 1), (900, 100), (800, 200), (700, 300), (600, 400), (501, 499)]
         self.budgets = [100+i*10 for i in range(4)]
 
     def tearDown(self):
+        self.base = "images/" + self.PATH
         x = []
         y1 = []
         y2 = []
@@ -79,10 +92,9 @@ class TestMeasureAllPayAuctionEffort(unittest.TestCase):
         ax2 = plt.subplot2grid((3, 1), (1, 0))
         ax3 = plt.subplot2grid((3, 1), (2, 0))
         for values in self.tests:
-            au = AllPayAuction(values, self.budgets, random_start=False)
+            au = AllPayAuction(values, self.budgets, random_start=self.RANDOM_START)
 
-            y_max, y_avg, y_min = au.iterative_best_response(False)
-            print(au.bids)
+            y_max, y_avg, y_min = au.iterative_best_response()
             x.append(abs(values[0] - values[1]))
             y1.append(y_max)
             y2.append(y_avg)
@@ -92,12 +104,21 @@ class TestMeasureAllPayAuctionEffort(unittest.TestCase):
         ax2.plot(x, y2, 'b')
         ax3.plot(x, y3, 'g')
 
-        plt.savefig(self.name+"-effort.png")
+        ax1.set_title("Maximum")
+        ax2.set_title("Average")
+        ax3.set_title("Minimum")
 
+        plt.savefig(self.base + self.name + "effort.png")
 
 
 if __name__ == "__main__":
-    #suite = unittest.TestLoader().loadTestsFromTestCase(TestBiddingAllPayAuction)
-    #unittest.TextTestRunner(verbosity=2).run(suite)
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestMeasureAllPayAuctionEffort)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    START_POINTS = [(True, "rstart/"), (False, "0start/")]
+    for _boolean, _path in START_POINTS:
+        TestBiddingAllPayAuction.RANDOM_START = _boolean
+        TestMeasureAllPayAuctionEffort.RANDOM_START = _boolean
+        TestBiddingAllPayAuction.PATH = _path
+        TestMeasureAllPayAuctionEffort.PATH = _path
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestBiddingAllPayAuction)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestMeasureAllPayAuctionEffort)
+        unittest.TextTestRunner(verbosity=2).run(suite)
